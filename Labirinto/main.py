180f053121856526a2bc75c554a69393a339d5f9
Spamp <antonio.spampinato@alcampus.it>
2023-05-23 20:37:58 +0200

    update main.py
    chiamata direttamente al metodo leggi_file che smista le chiamate dei file

--- Labirinto/main.py
+++ Labirinto/main.py
@@ -14,4 +14,4 @@ if __name__ == "__main__":
     print(lista_file)
     input_file=Input_file()
     filepath = './indata/'+str(input('Inserisci il nome del file da leggere con formato tiff o json tra uno di quelli elencati:  '))
-    leggiFile= input_file.leggi_file_tiff(filepath)
+    leggiFile= input_file.leggi_file(filepath)
