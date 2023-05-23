f32de3519df1a90e0f346f208163666d93338c42
ClaudiaMarano <118067217+ClaudiaMarano@users.noreply.github.com>
2023-05-23 13:44:19 +0200

    Creazione della classe input_file

---
+++ Labirinto/input_file.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 23 13:41:20 2023
+
+"""
+
+class Input_file:
+    
+    """
+    Classe che implementa e gestisce l'input
+    
+    Il programma riceve in ingresso:
+        - il file, in formato .json o .tiff
+        - il/i punto/i di partenza e arrivo
+        
+    """
