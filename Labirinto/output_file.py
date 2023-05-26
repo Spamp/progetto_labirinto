99473175fc27848b7406c8a9cdc6f90b6fddb138
Spamp <antonio.spampinato@alcampus.it>
2023-05-26 19:03:35 +0200

    update output_file.py

--- Labirinto/output_file.py
+++ Labirinto/output_file.py
@@ -2,6 +2,26 @@
 """
 Created on Fri May 26 18:04:49 2023
 
-@author: antos
 """
 
+class Output_file:
+    """
+    classe che implementa metodi di per la creazione dei file di output
+    """
+    def __init__(self, nomefile):
+        """
+        Costruttore della classe Output_file
+        
+        parameters
+        ----------
+        nomefile= string
+        
+        Returns
+        -------
+        None.
+        
+        """
+        self.percorso_cartella='./output'
+        self.nomefile=nomefile
+        self.percorso_file=self.percorso_cartella+'/'+self.nomefile
+        
